faab8425ef475bb166755d6a122ee37489105489
SeanJia <zjia@eng.ucsd.edu>
2023-02-12 18:27:23 -0800

    adjust camera

--- mani_skill2/agents/configs/xarm7/defaults.py
+++ mani_skill2/agents/configs/xarm7/defaults.py
@@ -124,7 +124,7 @@ class Xarm7DefaultConfig:
         return [
             CameraConfig(
                 uid="hand_camera",
-                p=[0.0, 0.0, 0.0],
+                p=[0.0, 0.0, -0.03],
                 q=euler2quat(0, -np.pi / 2, 0),
                 width=128,
                 height=128,
